2d251f2646f8a3ea68cf7917b3dfd5d50b59c1f4
Tom Rafferty <thomasrafferty@gmail.com>
2014-05-08 17:21:34 -0500

    added init.d startup script

---
+++ RPi_init.d_autostart.sh
@@ -0,0 +1,29 @@
+
+#! /bin/sh
+# /etc/init.d/garage_monitor 
+
+### BEGIN INIT INFO
+# Provides:          garage_monitor
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Starts the python script 'RPi_GarageMonitor' used to monitor the door and detect motion
+# Description:       Starts the python script 'RPi_GarageMonitor' used to monitor the door and detect motion
+### END INIT INFO
+
+case "$1" in
+  start)
+    echo "Starting RPi_GarageMonitor"
+    /usr/bin/python /home/pi/src/RPi_GPIOEventProcessor/RPi_GPIOEventProcessor.py -g /home/pi/src/RPi_GPIOEventProcessor/GPIO.json -e /home/pi/src/RPi_GPIOEventProcessor/EventTriggers.json
+    ;;
+  stop)
+    echo "Stopping RPi_GarageMonitor"
+    ;;
+  *)
+    echo "Usage: /etc/init.d/noip {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
