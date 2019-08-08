3d68666cd9beaccc7c62c8219629b225f4f7d5dd
Tom Rafferty <trafferty@cnt.canon.com>
2019-08-08 15:49:40 -0500

    added test script to do long-term testing of ESP module

---
+++ test_http_get_Action.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import signal
+import json
+import os.path
+import argparse
+import time
+import logging
+from actions import Actions
+
+(S_OFF, S_ON) = range(2)
+
+if __name__ == '__main__':
+
+    def sigint_handler(signal, frame):
+        logger.info("Caught Ctrl-c...")
+        eventMonitor.stop()
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    parser = argparse.ArgumentParser(description='Event monitor system for Raspberry Pi')
+    parser.add_argument('-d', '--duration_h', type=float, help='Test duration in hours', required=True)
+    parser.add_argument('-p', '--pause_s', default=30, type=int, help='Pause time between toggles (optional)')
+    parser.add_argument('-a', '--actions', type=str, help='JSON file defining the actions', required=True)
+    parser.add_argument('-l', '--log_file', type=str, default='~/action_test.txt', help='log file path for test (optional)', required=False)
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s.%(msecs)03d (%(name)10s) [%(levelname)7s]: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(os.path.expanduser(args.log_file), mode='a')
+        ])
+    logger = logging.getLogger("Test")
+
+    action_defs = json.load(open(args.actions, 'r'))
+
+    actions = Actions(action_defs)
+
+    test_duration_s = args.duration_h * 3600
+    logger.info("Test duration: %0.2f hours (%d sec)" % (args.duration, test_duration_s))
+
+    garageLights_state = S_OFF
+    def toggleGarageLight():
+        if garageLights_state == S_OFF:
+            if actions.processAction('garage_light_on'):
+                garageLights_state = S_ON
+                logger.info("turned garage light on")
+            else:
+                logger.warn("Error turning garage light on")
+        else:
+            if actions.processAction('garage_light_off'):
+                garageLights_state = S_OFF
+                logger.info("turned garage light off")
+            else:
+                logger.warn("Error turning garage light off")
+            logger.info("turning garage light off")
+
+    logger.info("Starting test...")
+    start_ts = time.time()
+    while start_ts+test_duration_s > time.time():
+        toggleGarageLight()
+        logger.info("Pausing for %ds..." % (args.pause_s))
+        time.sleep(args.pause_s)
+
+    logger.info("Test completed after %0.2f hours" % (args.duration))
